d866660b596292d66720377ff95575c21dfcb23b
Artem <artemoreh245@gmail.com>
2026-06-03 15:06:42 +0300

    Create pytest
    Added simple test function

---
+++ Лабораторная_работа_08/src/lab08_app/task03_simple_function.py
@@ -0,0 +1,5 @@
+"""Задание 3: простая функция для проверки pytest."""
+
+
+def add(first: float, second: float) -> float:
+    return first + second
---
+++ Лабораторная_работа_08/tests/test_task03_simple_function.py
@@ -0,0 +1,5 @@
+from lab08_app.task03_simple_function import add
+
+
+def test_add() -> None:
+    assert add(2, 3) == 5
